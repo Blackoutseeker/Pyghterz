1b6bbc1312a4dabf091ffb457fe1f691697ac224
Anderson Souza <Anderson1231993@hotmail.com>
2024-09-22 16:56:27 -0300

    feat:(main) modify main file to use class moving and fuction update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+from moving import Moving
 
 pygame.init()
 
@@ -10,12 +11,17 @@ pygame.display.set_caption('Pygtherz')
 
 FPS = 60
 clock = pygame.time.Clock()
+moving = Moving()
+
+img = pygame.image.load("C:\\Python\\Pyghterz\\assets\\images\\sprites\\characters\\RYU\\IDLE\\0.png")
 
 
 def game_loop():
     running = True
     while running:
         for event in pygame.event.get():
+            delta_time = clock.get_time()
+            moving.update(delta_time)
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
@@ -23,6 +29,7 @@ def game_loop():
                     running = False
 
         screen.fill((0, 0, 0,))
+        screen.blit(img, (moving.position_x, moving.position_y))
         pygame.display.flip()
         clock.tick(FPS)
     pygame.quit()
