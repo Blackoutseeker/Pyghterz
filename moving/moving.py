821c6579019d4c629ee160d2b9d8bd440e82a299
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-09-26 05:08:06 -0300

    feat(moving): change PlayerState by keys

--- moving/moving.py
+++ moving/moving.py
@@ -1,23 +1,27 @@
 import pygame
+from state import PlayerState
+from utils import PlayerAction
 
-clock = pygame.time.Clock()
-FPS = 60
-speed = 0.8
+speed = 700
 
 
 class Moving:
-    def __init__(self):
+    def __init__(self, player_state: PlayerState):
         self.position_x = 0
         self.position_y = 0
+        self.player_state: PlayerState = player_state
 
     def update(self, delta_time):
-
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
             self.position_y -= speed * delta_time
-        if keys[pygame.K_s]:
+        elif keys[pygame.K_s]:
             self.position_y += speed * delta_time
-        if keys[pygame.K_a]:
+        elif keys[pygame.K_a]:
             self.position_x -= speed * delta_time
-        if keys[pygame.K_d]:
+            self.player_state.set_player_action(PlayerAction.MOVE_BACKWARD)
+        elif keys[pygame.K_d]:
             self.position_x += speed * delta_time
+            self.player_state.set_player_action(PlayerAction.MOVE_FORWARD)
+        else:
+            self.player_state.set_player_action(PlayerAction.IDLE)
