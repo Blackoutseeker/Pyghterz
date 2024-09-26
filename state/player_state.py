2395458004b73fdeb42e08bcc637e8bb29535727
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-09-26 05:07:15 -0300

    feat(state): create methods get and set to PlayerState class

--- state/player_state.py
+++ state/player_state.py
@@ -4,3 +4,9 @@ from utils import PlayerAction
 class PlayerState:
     def __init__(self):
         self._player_action: PlayerAction = PlayerAction.IDLE
+
+    def get_player_action(self) -> PlayerAction:
+        return self._player_action
+
+    def set_player_action(self, player_action: PlayerAction):
+        self._player_action = player_action
