b6779329252e77547205c3bbe9a1de993f88993d
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-09-29 14:51:15 -0300

    feat(state): create is_attacking state to PlayerState

--- state/player_state.py
+++ state/player_state.py
@@ -4,9 +4,16 @@ from utils import PlayerAction
 class PlayerState:
     def __init__(self):
         self._player_action: PlayerAction = PlayerAction.IDLE
+        self._is_attacking: bool = False
 
     def get_player_action(self) -> PlayerAction:
         return self._player_action
 
     def set_player_action(self, player_action: PlayerAction):
         self._player_action = player_action
+
+    def get_is_attacking(self) -> bool:
+        return self._is_attacking
+
+    def set_is_attacking(self, is_attacking: bool):
+        self._is_attacking = is_attacking
