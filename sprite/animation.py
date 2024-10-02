c876af9311bf55625a66ea8987c0c1fa079c64c4
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-10-01 22:55:23 -0300

    refactor(sprite): encapsulate attributes from Animation class

--- sprite/animation.py
+++ sprite/animation.py
@@ -7,37 +7,37 @@ from typing import List
 
 class Animation:
     def __init__(self, character: Character, scale: float, speed: float, screen: Surface, player_state: PlayerState):
-        self.character: Character = character
-        self.scale: float = scale
-        self.speed: float = speed
-        self.screen: Surface = screen
-        self.player_state: PlayerState = player_state
+        self._character: Character = character
+        self._scale: float = scale
+        self._speed: float = speed
+        self._screen: Surface = screen
+        self._player_state: PlayerState = player_state
         self._sprite_index: int = 0
-        self.sprites: dict = {}
+        self._sprites: dict = {}
         for action in PlayerAction:
-            self.sprites[action.name] = self._load_sprites(action)
+            self._sprites[action.name] = self._load_sprites(action)
 
     def _load_sprites(self, player_action: PlayerAction) -> List[Surface]:
         sprites: List[Surface] = []
         base_path = path.dirname(path.dirname(__file__))
-        sprites_path: str = f'assets/images/sprites/characters/{self.character.name}/{player_action.name}'
+        sprites_path: str = f'assets/images/sprites/characters/{self._character.name}/{player_action.name}'
         sprites_path = path.join(base_path, sprites_path)
         files: List[str] = listdir(sprites_path)
         for file in files:
             sprite: Surface = image.load(f'{sprites_path}/{file}')
             width, height = sprite.get_size()
-            scaled_sprite: Surface = transform.scale(sprite, (int(width * self.scale), int(height * self.scale)))
+            scaled_sprite: Surface = transform.scale(sprite, (int(width * self._scale), int(height * self._scale)))
             sprites.append(scaled_sprite)
         return sprites
 
     def render(self, position_x: float, position_y: float):
-        self._sprite_index += self.speed
-        current_sprites: List[Surface] = self.sprites[self.player_state.get_player_action().name]
-        is_player_attacking: bool = self.player_state.get_is_attacking()
+        self._sprite_index += self._speed
+        current_sprites: List[Surface] = self._sprites[self._player_state.get_player_action().name]
+        is_player_attacking: bool = self._player_state.get_is_attacking()
         if is_player_attacking is True:
             if int(self._sprite_index) >= len(current_sprites):
-                self.player_state.set_is_attacking(False)
+                self._player_state.set_is_attacking(False)
                 self._sprite_index = 0
         if self._sprite_index >= len(current_sprites):
             self._sprite_index = 0
-        self.screen.blit(current_sprites[int(self._sprite_index)], (position_x, position_y))
+        self._screen.blit(current_sprites[int(self._sprite_index)], (position_x, position_y))
