0c7d27665fca0e7f018d6be942bc546347d97233
Anderson Souza <Anderson1231993@hotmail.com>
2024-10-05 00:47:16 -0300

    feat(utils): Add Dimetions to __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
 from .character import Character
 from .player_action import PlayerAction
+from .dimentions import Dimensions
 
-__all__ = ['Character', 'PlayerAction']
+__all__ = ['Character', 'PlayerAction', 'Dimensions']
