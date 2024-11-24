2806b3755089e4710ca5c8837ec8eab5e9b30425
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-11-24 12:45:05 -0300

    chore(utils): add MovementSpeed, AttackDamage and MoveSetStats importations to __init__.py file

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,5 +2,13 @@ from .character import Character
 from .player_action import PlayerAction
 from .dimentions import Dimensions
 from .config import Config
+from .moveset_stats import MoveSetStats, MovementSpeed, AttackDamage
 
-__all__ = ['Character', 'PlayerAction', 'Dimensions', 'Config']
+__all__ = ['Character',
+           'PlayerAction',
+           'Dimensions',
+           'Config',
+           'MoveSetStats',
+           'MovementSpeed',
+           'AttackDamage'
+           ]
