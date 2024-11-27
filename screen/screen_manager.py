a2975a43e24ba0c4b72d98031d767aa95ce8d9d2
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-11-26 21:44:18 -0300

    refactor(screen): create player's quiz state

--- screen/screen_manager.py
+++ screen/screen_manager.py
@@ -1,4 +1,6 @@
 from pygame import Surface
+from state import QuizState
+from utils import Character
 from .screen import ScreenType
 from .main_menu_screen import MainMenuScreen
 from .gameplay_screen import GameplayScreen
@@ -10,10 +12,12 @@ from pygame.event import Event
 class ScreenManager:
     def __init__(self, screen: Surface):
         self.screen = screen
+        self.player1_quiz = QuizState(Character.RYU)
+        self.player2_quiz = QuizState(Character.RYU, True)
         self._screens: dict = {
             f'{ScreenType.MAIN_MENU.name}': MainMenuScreen(screen),
-            f'{ScreenType.GAMEPLAY.name}': GameplayScreen(screen),
-            f'{ScreenType.QUIZ.name}': QuizScreen(screen),
+            f'{ScreenType.GAMEPLAY.name}': GameplayScreen(screen, self.player1_quiz, self.player2_quiz),
+            f'{ScreenType.QUIZ.name}': QuizScreen(screen, self.player1_quiz, self.player2_quiz),
         }
         self._current_screen: str = ScreenType.MAIN_MENU.name
 
